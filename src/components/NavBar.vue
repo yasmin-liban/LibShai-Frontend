733918980820c7bd12b2485e32173ae1da58ad0e
min <flor.yasmin90@gmail.com>
2022-04-15 18:34:00 -0300

    beginning ember

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <nav>
+    <span>Yasmin esteve aqui</span>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+  span {
+    background-color: aqua;
+  }
+</style>
